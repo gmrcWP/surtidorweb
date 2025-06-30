4d120830e65fcb33ae5ca3e571e4c6d45ae22a5a
alberttho1707666 <alberttho1707666@gmail.com>
2025-06-30 18:54:04 -0400

    Reporte de Comisiones o Bonificaciones por Personal

--- routes/ventaRoute.js
+++ routes/ventaRoute.js
@@ -3,5 +3,6 @@ const route = express.Router();
 const ventaControlador = require("../controllers/ventaController");
 
 route.get("/reporte-ingresos-surtidor", ventaControlador.reporteIngresosPorSurtidor);
+route.get("/reporte-comisiones-personal", ventaControlador.reporteComisionPersonal);
 
 module.exports = route;
